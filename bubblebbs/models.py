40bdba0bd4c7e9c473f55faf5445e343808bee6a
Lily Mayfield <lily.m.mayfield@gmail.com>
2018-05-09 02:35:59 -0500

    fenced code blocks

--- bubblebbs/models.py
+++ bubblebbs/models.py
@@ -125,6 +125,7 @@ class Post(db.Model):
                 'markdown.extensions.toc',
                 'markdown.extensions.def_list',
                 'markdown.extensions.abbr',
+                'markdown.extensions.fenced_code_blocks',
             ],
         )
         return md.convert(message)
